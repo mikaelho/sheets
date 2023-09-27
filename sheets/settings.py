5d66c8588e8c12884ff9579294ed048048763835
Mikael Honkala <mikael.honkala@gmail.com>
2023-09-27 21:25:19 +0300

    Update settings.py

--- sheets/settings.py
+++ sheets/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
