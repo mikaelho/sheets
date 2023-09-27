c52d15834d5abad3d380839055cb5eef238420a8
Mikael Honkala <mikael.honkala@gmail.com>
2023-09-27 20:57:14 +0300

    Add prod conf

--- sheets/settings.py
+++ sheets/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5*8(fnskga_dd9w#+)to#a$_8!n@b#j5n%-jxfjm-m4co@)*%%"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-5*8(fnskga_dd9w#+)to#a$_8!n@b#j5n%-jxfjm-m4co@)*%%")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = not bool(os.environ.get("SECRET_KEY", False))
 
 ALLOWED_HOSTS = [
     ".ngrok-free.app",
